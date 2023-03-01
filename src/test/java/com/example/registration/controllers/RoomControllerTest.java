d3de5012e6d28a525605844f8cda9122402864fb
Dmitriy Kravchuk <dmitriy.n.kravchuk@gmail.com>
2023-03-01 23:28:29 +0200

    sex with Mockito and JUnit

--- src/test/java/com/example/registration/controllers/RoomControllerTest.java
+++ src/test/java/com/example/registration/controllers/RoomControllerTest.java
@@ -19,13 +19,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+<<<<<<< HEAD
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+=======
+>>>>>>> 2950dcf (sex with Mockito and JUnit)
 
 @SpringJUnitConfig
 class RoomControllerTest {
 
     @Mock
+<<<<<<< HEAD
+=======
+    private RoomRepository roomRepository;
+    @Mock
+>>>>>>> 2950dcf (sex with Mockito and JUnit)
     private UserService userService;
     @Mock
     private OrderService orderService;
@@ -51,12 +59,15 @@ class RoomControllerTest {
 
     @Test
     void getRoomById() {
+<<<<<<< HEAD
         Mockito.when(roomService.findById(1L)).thenReturn(new Room(1L, 1, BigDecimal.valueOf(100), "good room"));
 
         Room room = roomService.findById(1L);
         assertEquals("good room", room.getTitle());
         assertEquals(1, room.getNumber());
         assertEquals(BigDecimal.valueOf(100), room.getPrice());
+=======
+>>>>>>> 2950dcf (sex with Mockito and JUnit)
     }
 
     @Test
